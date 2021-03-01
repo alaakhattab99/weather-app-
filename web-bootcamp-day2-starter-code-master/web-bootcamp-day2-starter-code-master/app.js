686fb374acfcad8aa196e9cfe7276a00e066ab39
Alaa Tariq Khattab <78685764+alaakhattab99@users.noreply.github.com>
2021-03-01 19:12:22 +0300

    last update

---
+++ web-bootcamp-day2-starter-code-master/web-bootcamp-day2-starter-code-master/app.js
@@ -0,0 +1,53 @@
+//Form
+var button= document.querySelector('.icon'); 
+var input= document.querySelector('#city-input'); 
+//City & weather
+var city= document.querySelector('#city'); 
+var weather= document.querySelector('#weather');
+//Temperature
+var currentTemp= document.querySelector('#current');
+var minTemp= document.querySelector('#min');
+var maxTemp= document.querySelector('#max');
+//Info
+var wind= document.querySelector('#wind');
+var humidity= document.querySelector('#humidity');
+var pressure= document.querySelector('#pressure');
+var icon= document.querySelector('#weather-icon');
+
+//Open weather API key: 
+const apiKey= '300d8940d5dc93efe72255df248f3e35'; 
+
+//Button Event listener 'click' 
+button.addEventListener('click', function(){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=300d8940d5dc93efe72255df248f3e35`)
+        .then(function(response){ return response.json()})
+        .then(function(data){
+    console.log(data)
+
+
+//Variables
+const city = data.name; 
+const weather= data.weather[0].description;
+const temp= Math.floor(data.main.temp);
+const minTemp= Math.floor(data.main.temp_min);
+const maxTemp= Math.floor(data.main.temp_max);
+const wind= data.wind.speed ; 
+const pressure= data.main.pressure ; 
+const humidity= data.main.humidity ; 
+const icon= data.weather[0].icon;
+const iconSrc= `http://openweathermap.org/img/wn/02d@2x.png`;
+
+
+    //place the value
+    window.city.innerHTML= city;
+    window.weather.innerHTML= weather; 
+    window.currentTemp.innerHTML= `${temp}<sup>°</sup>`; 
+    window.minTemp.innerHTML= `${minTemp}<sup>°</sup>`
+    window.maxTemp.innerHTML= `${maxTemp}<sup>°</sup>`
+    window.wind.innerHTML= speed ;
+    window.humidity.innerHTML= humidity;
+    window.pressure.innerHTML= pressure;
+    window.icon.src= `http://openweathermap.org/img/wn/02d@2x.png`;  
+    
+})
+})
